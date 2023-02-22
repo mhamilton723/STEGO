dd43a0da343dd7be331f2d1e8ed18d66b390fd31
Matthew Clapp <itsayellow+dev@gmail.com>
2023-02-22 15:04:13 -0800

    Use pathlib and fix path logic.

--- src/download_datasets.py
+++ src/download_datasets.py
@@ -1,27 +1,30 @@
-from utils import *
+from pathlib import Path
+
 import hydra
-from omegaconf import DictConfig
-import os
 import wget
+from omegaconf import DictConfig
+
+from utils import *
 
 
 @hydra.main(config_path="configs", config_name="eval_config.yml")
 def my_app(cfg: DictConfig) -> None:
-    pytorch_data_dir = cfg.pytorch_data_dir
+    pytorch_data_dir = Path(cfg.pytorch_data_dir)
     dataset_names = ["potsdam", "cityscapes", "cocostuff", "potsdamraw"]
     url_base = (
         "https://marhamilresearch4.blob.core.windows.net/stego-public/pytorch_data/"
     )
 
-    os.makedirs(pytorch_data_dir, exist_ok=True)
+    pytorch_data_dir.mkdir(exist_ok=True)
     for dataset_name in dataset_names:
-        if (not os.path.exists(join(pytorch_data_dir, dataset_name))) or (
-            not os.path.exists(join(pytorch_data_dir, dataset_name + ".zip"))
+        if not (
+            (pytorch_data_dir / dataset_name).exists()
+            or (pytorch_data_dir / f"{dataset_name}.zip").exists()
         ):
             print("\n Downloading {}".format(dataset_name))
             wget.download(
                 url_base + dataset_name + ".zip",
-                join(pytorch_data_dir, dataset_name + ".zip"),
+                pytorch_data_dir / f"{dataset_name}.zip",
             )
         else:
             print("\n Found {}, skipping download".format(dataset_name))
