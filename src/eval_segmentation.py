7437416baad44d5a58c41a8f2586163967d215d1
Matthew Clapp <itsayellow+dev@gmail.com>
2023-02-27 15:54:56 -0800

    black and isort reformat.

--- src/eval_segmentation.py
+++ src/eval_segmentation.py
@@ -1,31 +1,34 @@
-from modules import *
-from data import *
 from collections import defaultdict
 from multiprocessing import Pool
+
 import hydra
 import seaborn as sns
 import torch.multiprocessing
-from crf import dense_crf
 from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from train_segmentation import LitUnsupervisedSegmenter, prep_for_plot, get_class_labels
 
-torch.multiprocessing.set_sharing_strategy('file_system')
+from crf import dense_crf
+from data import *
+from modules import *
+from train_segmentation import LitUnsupervisedSegmenter, get_class_labels, prep_for_plot
+
+torch.multiprocessing.set_sharing_strategy("file_system")
+
 
 def plot_cm(histogram, label_cmap, cfg):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.gca()
     hist = histogram.detach().cpu().to(torch.float32)
     hist /= torch.clamp_min(hist.sum(dim=0, keepdim=True), 1)
-    sns.heatmap(hist.t(), annot=False, fmt='g', ax=ax, cmap="Blues", cbar=False)
-    ax.set_title('Predicted labels', fontsize=28)
-    ax.set_ylabel('True labels', fontsize=28)
+    sns.heatmap(hist.t(), annot=False, fmt="g", ax=ax, cmap="Blues", cbar=False)
+    ax.set_title("Predicted labels", fontsize=28)
+    ax.set_ylabel("True labels", fontsize=28)
     names = get_class_labels(cfg.dataset_name)
     if cfg.extra_clusters:
         names = names + ["Extra"]
-    ax.set_xticks(np.arange(0, len(names)) + .5)
-    ax.set_yticks(np.arange(0, len(names)) + .5)
+    ax.set_xticks(np.arange(0, len(names)) + 0.5)
+    ax.set_yticks(np.arange(0, len(names)) + 0.5)
     ax.xaxis.tick_top()
     ax.xaxis.set_ticklabels(names, fontsize=18)
     ax.yaxis.set_ticklabels(names, fontsize=18)
@@ -34,15 +37,15 @@ def plot_cm(histogram, label_cmap, cfg):
     [t.set_color(colors[i]) for i, t in enumerate(ax.yaxis.get_ticklabels())]
     plt.xticks(rotation=90)
     plt.yticks(rotation=0)
-    ax.vlines(np.arange(0, len(names) + 1), color=[.5, .5, .5], *ax.get_xlim())
-    ax.hlines(np.arange(0, len(names) + 1), color=[.5, .5, .5], *ax.get_ylim())
+    ax.vlines(np.arange(0, len(names) + 1), color=[0.5, 0.5, 0.5], *ax.get_xlim())
+    ax.hlines(np.arange(0, len(names) + 1), color=[0.5, 0.5, 0.5], *ax.get_ylim())
     plt.tight_layout()
 
 
 def batch_list(iterable, n=1):
     l = len(iterable)
     for ndx in range(0, l, n):
-        yield iterable[ndx:min(ndx + n, l)]
+        yield iterable[ndx : min(ndx + n, l)]
 
 
 def _apply_crf(tup):
@@ -50,7 +53,9 @@ def _apply_crf(tup):
 
 
 def batched_crf(pool, img_tensor, prob_tensor):
-    outputs = pool.map(_apply_crf, zip(img_tensor.detach().cpu(), prob_tensor.detach().cpu()))
+    outputs = pool.map(
+        _apply_crf, zip(img_tensor.detach().cpu(), prob_tensor.detach().cpu())
+    )
     return torch.cat([torch.from_numpy(arr).unsqueeze(0) for arr in outputs], dim=0)
 
 
@@ -85,9 +90,14 @@ def my_app(cfg: DictConfig) -> None:
             cfg=model.cfg,
         )
 
-        test_loader = DataLoader(test_dataset, cfg.batch_size * 2,
-                                 shuffle=False, num_workers=cfg.num_workers,
-                                 pin_memory=True, collate_fn=flexible_collate)
+        test_loader = DataLoader(
+            test_dataset,
+            cfg.batch_size * 2,
+            shuffle=False,
+            num_workers=cfg.num_workers,
+            pin_memory=True,
+            collate_fn=flexible_collate,
+        )
 
         model.eval().cuda()
 
@@ -112,7 +122,9 @@ def my_app(cfg: DictConfig) -> None:
         else:
             raise ValueError("Unknown Dataset {}".format(model.cfg.dataset_name))
         batch_nums = torch.tensor([n // (cfg.batch_size * 2) for n in all_good_images])
-        batch_offsets = torch.tensor([n % (cfg.batch_size * 2) for n in all_good_images])
+        batch_offsets = torch.tensor(
+            [n % (cfg.batch_size * 2) for n in all_good_images]
+        )
 
         saved_data = defaultdict(list)
         with Pool(cfg.num_workers + 5) as pool:
@@ -125,14 +137,20 @@ def my_app(cfg: DictConfig) -> None:
                     feats, code2 = par_model(img.flip(dims=[3]))
                     code = (code1 + code2.flip(dims=[3])) / 2
 
-                    code = F.interpolate(code, label.shape[-2:], mode='bilinear', align_corners=False)
+                    code = F.interpolate(
+                        code, label.shape[-2:], mode="bilinear", align_corners=False
+                    )
 
                     linear_probs = torch.log_softmax(model.linear_probe(code), dim=1)
                     cluster_probs = model.cluster_probe(code, 2, log_probs=True)
 
                     if cfg.run_crf:
-                        linear_preds = batched_crf(pool, img, linear_probs).argmax(1).cuda()
-                        cluster_preds = batched_crf(pool, img, cluster_probs).argmax(1).cuda()
+                        linear_preds = (
+                            batched_crf(pool, img, linear_probs).argmax(1).cuda()
+                        )
+                        cluster_preds = (
+                            batched_crf(pool, img, cluster_probs).argmax(1).cuda()
+                        )
                     else:
                         linear_preds = linear_probs.argmax(1)
                         cluster_preds = cluster_probs.argmax(1)
@@ -142,17 +160,24 @@ def my_app(cfg: DictConfig) -> None:
 
                     if run_picie:
                         picie_preds = picie_cluster_metrics.map_clusters(
-                            picie_cluster_probe(par_picie(img), None)[1].argmax(1).cpu())
+                            picie_cluster_probe(par_picie(img), None)[1].argmax(1).cpu()
+                        )
 
                     if i in batch_nums:
                         matching_offsets = batch_offsets[torch.where(batch_nums == i)]
                         for offset in matching_offsets:
-                            saved_data["linear_preds"].append(linear_preds.cpu()[offset].unsqueeze(0))
-                            saved_data["cluster_preds"].append(cluster_preds.cpu()[offset].unsqueeze(0))
+                            saved_data["linear_preds"].append(
+                                linear_preds.cpu()[offset].unsqueeze(0)
+                            )
+                            saved_data["cluster_preds"].append(
+                                cluster_preds.cpu()[offset].unsqueeze(0)
+                            )
                             saved_data["label"].append(label.cpu()[offset].unsqueeze(0))
                             saved_data["img"].append(img.cpu()[offset].unsqueeze(0))
                             if run_picie:
-                                saved_data["picie_preds"].append(picie_preds.cpu()[offset].unsqueeze(0))
+                                saved_data["picie_preds"].append(
+                                    picie_preds.cpu()[offset].unsqueeze(0)
+                                )
         saved_data = {k: torch.cat(v, dim=0) for k, v in saved_data.items()}
 
         tb_metrics = {
@@ -173,29 +198,50 @@ def my_app(cfg: DictConfig) -> None:
             n_rows += 1
 
         if cfg.dark_mode:
-            plt.style.use('dark_background')
+            plt.style.use("dark_background")
 
         for good_images in batch_list(range(len(all_good_images)), 10):
-            fig, ax = plt.subplots(n_rows, len(good_images), figsize=(len(good_images) * 3, n_rows * 3))
+            fig, ax = plt.subplots(
+                n_rows, len(good_images), figsize=(len(good_images) * 3, n_rows * 3)
+            )
             for i, img_num in enumerate(good_images):
-                plot_img = (prep_for_plot(saved_data["img"][img_num]) * 255).numpy().astype(np.uint8)
-                plot_label = (model.label_cmap[saved_data["label"][img_num]]).astype(np.uint8)
-                Image.fromarray(plot_img).save(join(join(result_dir, "img", str(img_num) + ".jpg")))
-                Image.fromarray(plot_label).save(join(join(result_dir, "label", str(img_num) + ".png")))
+                plot_img = (
+                    (prep_for_plot(saved_data["img"][img_num]) * 255)
+                    .numpy()
+                    .astype(np.uint8)
+                )
+                plot_label = (model.label_cmap[saved_data["label"][img_num]]).astype(
+                    np.uint8
+                )
+                Image.fromarray(plot_img).save(
+                    join(join(result_dir, "img", str(img_num) + ".jpg"))
+                )
+                Image.fromarray(plot_label).save(
+                    join(join(result_dir, "label", str(img_num) + ".png"))
+                )
 
                 ax[0, i].imshow(plot_img)
                 ax[1, i].imshow(plot_label)
                 if cfg.run_prediction:
-                    plot_cluster = (model.label_cmap[
-                        model.test_cluster_metrics.map_clusters(
-                            saved_data["cluster_preds"][img_num])]) \
-                        .astype(np.uint8)
-                    Image.fromarray(plot_cluster).save(join(join(result_dir, "cluster", str(img_num) + ".png")))
+                    plot_cluster = (
+                        model.label_cmap[
+                            model.test_cluster_metrics.map_clusters(
+                                saved_data["cluster_preds"][img_num]
+                            )
+                        ]
+                    ).astype(np.uint8)
+                    Image.fromarray(plot_cluster).save(
+                        join(join(result_dir, "cluster", str(img_num) + ".png"))
+                    )
                     ax[2, i].imshow(plot_cluster)
                 if run_picie:
-                    picie_img = model.label_cmap[saved_data["picie_preds"][img_num]].astype(np.uint8)
+                    picie_img = model.label_cmap[
+                        saved_data["picie_preds"][img_num]
+                    ].astype(np.uint8)
                     ax[3, i].imshow(picie_img)
-                    Image.fromarray(picie_img).save(join(join(result_dir, "picie", str(img_num) + ".png")))
+                    Image.fromarray(picie_img).save(
+                        join(join(result_dir, "picie", str(img_num) + ".png"))
+                    )
 
             ax[0, 0].set_ylabel("Image", fontsize=26)
             ax[1, 0].set_ylabel("Label", fontsize=26)
