7d66d6cfe1df858d708355ccd013419ea7fbecbd
Matthew Clapp <itsayellow+dev@gmail.com>
2023-03-08 23:19:49 -0800

    Black reformat and isort.

--- src/precompute_knns.py
+++ src/precompute_knns.py
@@ -1,15 +1,17 @@
-from data import ContrastiveSegDataset
-from modules import *
 import os
 from os.path import join
+
 import hydra
 import numpy as np
 import torch.multiprocessing
-import torch.multiprocessing
-import torch.nn as nn
+import torch.nn.functional as F
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning.utilities.seed import seed_everything
+from torch.utils.data import DataLoader
 from tqdm import tqdm
+
+from data import ContrastiveSegDataset
+from utils import get_transform, load_model, prep_args
 
 
 def get_feats(model, loader):
@@ -41,30 +43,39 @@ def my_app(cfg: DictConfig) -> None:
     crop_types = ["five", None]
 
     # Uncomment these lines to run on custom datasets
-    #dataset_names = ["directory"]
-    #crop_types = [None]
+    # dataset_names = ["directory"]
+    # crop_types = [None]
 
     res = 224
     n_batches = 16
 
     if cfg.arch == "dino":
         from modules import DinoFeaturizer, LambdaLayer
+
         no_ap_model = torch.nn.Sequential(
-            DinoFeaturizer(20, cfg),  # dim doesent matter
-            LambdaLayer(lambda p: p[0]),
+            DinoFeaturizer(20, cfg), LambdaLayer(lambda p: p[0])  # dim doesent matter
         ).cuda()
     else:
         cut_model = load_model(cfg.model_type, join(cfg.output_root, "data")).cuda()
-        no_ap_model = nn.Sequential(*list(cut_model.children())[:-1]).cuda()
+        no_ap_model = torch.nn.Sequential(*list(cut_model.children())[:-1]).cuda()
     par_model = torch.nn.DataParallel(no_ap_model)
 
     for crop_type in crop_types:
         for image_set in image_sets:
             for dataset_name in dataset_names:
-                nice_dataset_name = cfg.dir_dataset_name if dataset_name == "directory" else dataset_name
+                nice_dataset_name = (
+                    cfg.dir_dataset_name
+                    if dataset_name == "directory"
+                    else dataset_name
+                )
 
-                feature_cache_file = join(pytorch_data_dir, "nns", "nns_{}_{}_{}_{}_{}.npz".format(
-                    cfg.model_type, nice_dataset_name, image_set, crop_type, res))
+                feature_cache_file = join(
+                    pytorch_data_dir,
+                    "nns",
+                    "nns_{}_{}_{}_{}_{}.npz".format(
+                        cfg.model_type, nice_dataset_name, image_set, crop_type, res
+                    ),
+                )
 
                 if not os.path.exists(feature_cache_file):
                     print("{} not found, computing".format(feature_cache_file))
@@ -78,7 +89,13 @@ def my_app(cfg: DictConfig) -> None:
                         cfg=cfg,
                     )
 
-                    loader = DataLoader(dataset, 256, shuffle=False, num_workers=cfg.num_workers, pin_memory=False)
+                    loader = DataLoader(
+                        dataset,
+                        256,
+                        shuffle=False,
+                        num_workers=cfg.num_workers,
+                        pin_memory=False,
+                    )
 
                     with torch.no_grad():
                         normed_feats = get_feats(par_model, loader)
@@ -87,13 +104,17 @@ def my_app(cfg: DictConfig) -> None:
                         print(normed_feats.shape)
                         for i in tqdm(range(0, normed_feats.shape[0], step)):
                             torch.cuda.empty_cache()
-                            batch_feats = normed_feats[i:i + step, :]
-                            pairwise_sims = torch.einsum("nf,mf->nm", batch_feats, normed_feats)
+                            batch_feats = normed_feats[i : i + step, :]
+                            pairwise_sims = torch.einsum(
+                                "nf,mf->nm", batch_feats, normed_feats
+                            )
                             all_nns.append(torch.topk(pairwise_sims, 30)[1])
                             del pairwise_sims
                         nearest_neighbors = torch.cat(all_nns, dim=0)
 
-                        np.savez_compressed(feature_cache_file, nns=nearest_neighbors.numpy())
+                        np.savez_compressed(
+                            feature_cache_file, nns=nearest_neighbors.numpy()
+                        )
                         print("Saved NNs", cfg.model_type, nice_dataset_name, image_set)
 
 
