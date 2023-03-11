55cbf2d1ef1469bad0de390c86b3d906eef71237
Matthew Clapp <itsayellow+dev@gmail.com>
2023-03-10 21:44:57 -0800

    black and isort code cleanup.

--- src/train_crf.py
+++ src/train_crf.py
@@ -1,25 +1,27 @@
 import io
+from datetime import datetime
 
-import PIL.Image
+import hydra
 import matplotlib.pyplot as plt
-import torch
-from tensorboardX import SummaryWriter
-from torch.nn import Sequential, Linear, LogSoftmax
-from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
-from utils import *
-from tqdm import tqdm
-from torch.utils.data import DataLoader
 import numpy as np
+import PIL.Image
+import torch
+from kornia.color import rgb_to_lab
+from omegaconf import DictConfig, OmegaConf
 from skimage.segmentation import mark_boundaries
 from sklearn.decomposition import PCA
-from kornia.color import rgb_to_lab
-from datetime import datetime
-import hydra
-from omegaconf import DictConfig, OmegaConf
+from tensorboardX import SummaryWriter
+from torch.nn import Linear, LogSoftmax, Sequential
+from torch.utils.data import DataLoader, Dataset
+from torchvision.transforms import ToTensor
+from tqdm import tqdm
+
+from utils import *
+
 
 def norm(t):
     return F.normalize(t, dim=1, eps=1e-10)
+
 
 def prep(continuous: bool, t: torch.Tensor):
     if continuous:
@@ -27,8 +29,9 @@ def prep(continuous: bool, t: torch.Tensor):
     else:
         return torch.exp(t)
 
+
 def entropy(p):
-    p = torch.clamp_min(p, .0000001)
+    p = torch.clamp_min(p, 0.0000001)
     return -(p * torch.log(p)).sum(dim=1)
 
 
@@ -46,22 +49,38 @@ def my_app(cfg: DictConfig) -> None:
     torch.random.manual_seed(0)
 
     small_imsize = imsize // 2
-    transform_with_resize = T.Compose([T.Resize((small_imsize, small_imsize)), T.ToTensor(), normalize])
-    label_transform_with_resize = T.Compose([T.Resize((small_imsize, small_imsize)), ToTargetTensor()])
+    transform_with_resize = T.Compose(
+        [T.Resize((small_imsize, small_imsize)), T.ToTensor(), normalize]
+    )
+    label_transform_with_resize = T.Compose(
+        [T.Resize((small_imsize, small_imsize)), ToTargetTensor()]
+    )
 
     dataset = ContrastiveSegDataset(
-        pytorch_data_dir, dataset_name, "train+val", cfg.num_neighbors,
-        transform_with_resize, label_transform_with_resize, None, None, cfg)
+        pytorch_data_dir,
+        dataset_name,
+        "train+val",
+        cfg.num_neighbors,
+        transform_with_resize,
+        label_transform_with_resize,
+        None,
+        None,
+        cfg,
+    )
 
     prefix = "crf/{}_{}".format(cfg.dataset_name, cfg.experiment_name)
     writer = SummaryWriter(
-        join(log_dir, '{}_date_{}'.format(prefix, datetime.now().strftime("%m:%d:%Y:%H:%M"))))
+        join(
+            log_dir,
+            "{}_date_{}".format(prefix, datetime.now().strftime("%m:%d:%Y:%H:%M")),
+        )
+    )
 
     class CodeSpaceTable(torch.nn.Module):
         def __init__(self, continuous, n_images, dim, h, w):
             super(CodeSpaceTable, self).__init__()
             self.continuous = continuous
-            self.code_space = torch.nn.Parameter(torch.randn(n_images, dim, h, w) * .1)
+            self.code_space = torch.nn.Parameter(torch.randn(n_images, dim, h, w) * 0.1)
 
         def forward(self, x):
             if self.continuous:
@@ -71,7 +90,7 @@ def my_app(cfg: DictConfig) -> None:
 
     def add_plot(writer, name, step):
         buf = io.BytesIO()
-        plt.savefig(buf, format='jpeg')
+        plt.savefig(buf, format="jpeg")
         buf.seek(0)
         image = PIL.Image.open(buf)
         image = ToTensor()(image)
@@ -92,15 +111,22 @@ def my_app(cfg: DictConfig) -> None:
     net = CodeSpaceTable(continuous, n_images, dim, img.shape[2], img.shape[3]).cuda()
     optim = torch.optim.Adam(list(net.parameters()), lr=1e-2)
 
-    loss_func = ContrastiveCRFLoss(cfg.crf_samples, cfg.alpha, cfg.beta, cfg.gamma, cfg.w1, cfg.w2, cfg.shift)
+    loss_func = ContrastiveCRFLoss(
+        cfg.crf_samples, cfg.alpha, cfg.beta, cfg.gamma, cfg.w1, cfg.w2, cfg.shift
+    )
 
     def to_normed_lab(img):
         img_t = rgb_to_lab(img)
-        img_t /= torch.tensor([100, 128 * 2, 128 * 2]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).cuda()
+        img_t /= (
+            torch.tensor([100, 128 * 2, 128 * 2])
+            .unsqueeze(0)
+            .unsqueeze(-1)
+            .unsqueeze(-1)
+            .cuda()
+        )
         return img_t
 
     for i in tqdm(range(cfg.epochs)):
-
         code = net.forward(img)
         if cfg.color_space == "rgb":
             img_t = img
@@ -114,12 +140,13 @@ def my_app(cfg: DictConfig) -> None:
         else:
             ent_global = entropy(torch.exp(code).mean(dim=0, keepdim=True)).mean()
             ent_local = entropy(torch.exp(code)).mean()
-            ent_reg_term = - cfg.global_ent_weight * ent_global \
-                           - cfg.local_ent_weight * ent_local
+            ent_reg_term = (
+                -cfg.global_ent_weight * ent_global - cfg.local_ent_weight * ent_local
+            )
 
             if i % 100 == 0:
-                writer.add_scalar('ent/ent1', ent_global, i)
-                writer.add_scalar('ent/ent2', ent_local, i)
+                writer.add_scalar("ent/ent1", ent_global, i)
+                writer.add_scalar("ent/ent2", ent_local, i)
 
         crf_loss = loss_func(img_t, prep(continuous, code))
         loss = crf_loss.mean() + ent_reg_term
@@ -137,14 +164,23 @@ def my_app(cfg: DictConfig) -> None:
             with torch.no_grad():
                 for idx, img_idx in enumerate(ind[:n_images]):
                     plot_img = unnorm(img)[idx].permute(1, 2, 0)
-                    plot_img = (plot_img - plot_img.min()) / (plot_img.max() - plot_img.min())
+                    plot_img = (plot_img - plot_img.min()) / (
+                        plot_img.max() - plot_img.min()
+                    )
                     ax[0, idx].imshow(plot_img.cpu())
                     if not continuous:
-                        ax[1, idx].imshow(mark_boundaries(plot_img.cpu(), code.argmax(1)[idx].cpu().numpy()))
+                        ax[1, idx].imshow(
+                            mark_boundaries(
+                                plot_img.cpu(), code.argmax(1)[idx].cpu().numpy()
+                            )
+                        )
                     else:
                         X_code = code[idx].permute(1, 2, 0).reshape(-1, dim).cpu()
-                        projected_code = PCA(n_components=3).fit_transform(X_code) \
+                        projected_code = (
+                            PCA(n_components=3)
+                            .fit_transform(X_code)
                             .reshape([code.shape[2], code.shape[3], 3])
+                        )
                         projected_code = (projected_code + 1) / 2
                         projected_code = np.clip(projected_code, 0, 1)
                         ax[1, idx].imshow(projected_code)
