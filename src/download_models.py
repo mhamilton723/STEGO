3e8d23e35bef9426b6513508ba4d58ed9e605133
Matthew Clapp <itsayellow+dev@gmail.com>
2023-03-15 12:29:53 -0700

    black and isort reformat.

--- src/download_models.py
+++ src/download_models.py
@@ -1,27 +1,39 @@
-from os.path import join, exists
-import wget
 import os
+from os.path import exists, join
+from pathlib import Path
+
+import wget
 
 models_dir = join("..", "models")
 os.makedirs(models_dir, exist_ok=True)
-model_url_root = "https://marhamilresearch4.blob.core.windows.net/stego-public/models/models/"
-model_names = ["moco_v2_800ep_pretrain.pth.tar",
-               "model_epoch_0720_iter_085000.pth",
-               "picie.pkl"]
+model_url_root = (
+    "https://marhamilresearch4.blob.core.windows.net/stego-public/models/models/"
+)
+model_names = [
+    "moco_v2_800ep_pretrain.pth.tar",
+    "model_epoch_0720_iter_085000.pth",
+    "picie.pkl",
+]
 
 saved_models_dir = join("..", "saved_models")
 os.makedirs(saved_models_dir, exist_ok=True)
-saved_model_url_root = "https://marhamilresearch4.blob.core.windows.net/stego-public/saved_models/"
-saved_model_names = ["cityscapes_vit_base_1.ckpt",
-                     "cocostuff27_vit_base_5.ckpt",
-                     "picie_and_probes.pth",
-                     "potsdam_test.ckpt"]
+saved_model_url_root = (
+    "https://marhamilresearch4.blob.core.windows.net/stego-public/saved_models/"
+)
+saved_model_names = [
+    "cityscapes_vit_base_1.ckpt",
+    "cocostuff27_vit_base_5.ckpt",
+    "picie_and_probes.pth",
+    "potsdam_test.ckpt",
+]
 
-target_files = [join(models_dir, mn) for mn in model_names] + \
-               [join(saved_models_dir, mn) for mn in saved_model_names]
+target_files = [join(models_dir, mn) for mn in model_names] + [
+    join(saved_models_dir, mn) for mn in saved_model_names
+]
 
-target_urls = [model_url_root + mn for mn in model_names] + \
-              [saved_model_url_root + mn for mn in saved_model_names]
+target_urls = [model_url_root + mn for mn in model_names] + [
+    saved_model_url_root + mn for mn in saved_model_names
+]
 
 for target_file, target_url in zip(target_files, target_urls):
     if not exists(target_file):
